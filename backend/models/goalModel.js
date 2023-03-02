a6fc19d813d5d43ac369ea169bd901cfdc00f55f
YUN-CHENG <608556007@alum.ccu.edu.tw>
2023-03-02 20:00:35 +0800

    update: Add JWT

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,11 @@
 const mongooose = require('mongoose')
 
 const goalSchema = mongooose.Schema({
+    user: {
+        type: mongooose.Schema.Types.ObjectId,
+        require: true,
+        ref: 'User'
+    },
     text: {
         type: String,
         required: [true, 'Please add a text value']
